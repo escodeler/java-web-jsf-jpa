3219a67871ceeea84dba58df8169b4cab68894da
Emilio Scodeler <scodeler.info@gmail.com>
2015-12-01 09:44:47 -0200

    First JSF page saving data

--- src/com/faces/motors/beans/CarBean.java
+++ src/com/faces/motors/beans/CarBean.java
@@ -1,7 +1,31 @@
 package com.faces.motors.beans;
 
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+
+import com.faces.motors.entities.Car;
+import com.faces.motors.utils.JPAUtil;
 
 @ManagedBean
 public class CarBean {
+	
+	private Car car = new Car();
+	
+	public Car getCar() {
+		return car;
+	}
+
+	public void setCar(Car car) {
+		this.car = car;
+	}
+
+	public void save(Car car) {
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(car);
+		em.getTransaction().commit();
+		
+		em.close();
+	}
 }
