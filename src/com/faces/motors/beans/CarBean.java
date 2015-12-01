a6dccf5830b09b5e301d3a60604ec2f2815da070
Emilio Scodeler <scodeler.info@gmail.com>
2015-12-01 12:04:57 -0200

    First version of CRUD OK

--- src/com/faces/motors/beans/CarBean.java
+++ src/com/faces/motors/beans/CarBean.java
@@ -1,7 +1,10 @@
 package com.faces.motors.beans;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import com.faces.motors.entities.Car;
 import com.faces.motors.utils.JPAUtil;
@@ -24,6 +27,34 @@ public class CarBean {
 		
 		em.getTransaction().begin();
 		em.persist(car);
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+	
+	private List<Car> cars;
+
+	@SuppressWarnings("unchecked")
+	public List<Car> getCars() {
+		
+		if(this.cars == null) {
+
+			EntityManager em = JPAUtil.getEntityManager();
+			Query q = em.createQuery("select c from Car c", Car.class);
+			
+			this.cars = q.getResultList();
+			
+			em.close();
+		}
+		return cars;
+	}
+	
+	public void remove(Car car) {
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		em.getTransaction().begin();
+		car = em.merge(car);
+		em.remove(car);
 		em.getTransaction().commit();
 		
 		em.close();
